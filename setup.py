690dc27774a34edb5dcc75079b3d46380bad0362
Miroslav Jirik <miroslav.jirik@gmail.com>
2018-09-11 11:09:40 +0200

    remove unused dependencies

--- setup.py
+++ setup.py
@@ -35,7 +35,7 @@ setup(
         # Specify the Python versions you support here. In particular, ensure
         # that you indicate whether you support Python 2, Python 3 or both.
         # 'Programming Language :: Python :: 2',
-        # 'Programming Language :: Python :: 2.6',
+        'Programming Language :: Python :: 2.6',
         'Programming Language :: Python :: 2.7',
         # 'Programming Language :: Python :: 3',
         # 'Programming Language :: Python :: 3.2',
@@ -48,13 +48,13 @@ setup(
 
     # You can just specify the packages manually here if your project is
     # simple. Or you can use find_packages().
-    packages=find_packages(exclude=['dist',  'docs', 'tests*']),
+    packages=find_packages(exclude=['dist',  'docs', 'tests*', 'graphics']),
 
     # List run-time dependencies here.  These will be installed by pip when
     # your project is installed. For an analysis of "install_requires" vs pip's
     # requirements files see:
     # https://packaging.python.org/en/latest/technical.html#install-requires-vs-requirements-files
-    install_requires=['numpy', 'conda'],
+    install_requires=[],
     # 'SimpleITK'],  # Removed becaouse of errors when pip is installing
     dependency_links=[],
 
