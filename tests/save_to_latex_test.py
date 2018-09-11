76a9e0e8244da8a385674babd2df18ec29595c8b
Miroslav Jirik <miroslav.jirik@gmail.com>
2018-09-11 22:45:17 +0200

    debug print glob

--- tests/save_to_latex_test.py
+++ tests/save_to_latex_test.py
@@ -62,7 +62,7 @@ class DataToTexTest(unittest.TestCase):
         if op.exists(pdffile):
             os.remove(pdffile)
         import glob
-        print(glob.glob(pth))
+        print(glob.glob(op.join(pth, "*")))
         subprocess.check_call("pdflatex test_latex.tex -interaction nonstopmode", shell=True, cwd=pth)
         # subprocess.run("dir", shell=True)
         self.assertTrue(op.exists(pdffile))
