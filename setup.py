aeacf9e03e2fcdb8c87acb337ae73a9c60ea4a20
Miroslav Jirik <miroslav.jirik@gmail.com>
2019-02-18 17:11:45 +0100

    Bump version: 0.1.0 → 0.2.0

--- setup.py
+++ setup.py
@@ -11,7 +11,7 @@ setup(
     # Versions should comply with PEP440.  For a discussion on single-sourcing
     # the version across setup.py and the project code, see
     # http://packaging.python.org/en/latest/tutorial.html#version
-    version='0.1.0',
+    version='0.2.0',
     url='https://github.com//data2tex',
     author='',
     author_email='',
